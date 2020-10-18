75639302f6139408e03d9557a9a04a26bb2fd63b
SeeBasTStick <sebastian.stock@hhu.de>
2020-10-18 18:38:12 +0200

    added messages as error type

--- src/main/kotlin/b/language/server/proBMangement/prob/Util.kt
+++ src/main/kotlin/b/language/server/proBMangement/prob/Util.kt
@@ -37,9 +37,9 @@ fun getErrorItemType(errorItem: ErrorItem.Type) : DiagnosticSeverity{
         ErrorItem.Type.INTERNAL_ERROR -> {
             DiagnosticSeverity.Error
         }
-     /*   ErrorItem.Type.MESSAGE -> {
+        ErrorItem.Type.MESSAGE -> {
             DiagnosticSeverity.Information
-        }*/
+        }
         else -> {
             DiagnosticSeverity.Error
         }
