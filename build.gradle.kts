59f9cbfab26977e24b73e248c51d874628f04636
dgelessus <dgelessus@users.noreply.github.com>
2023-06-26 15:44:46 +0200

    Set Java versions explicitly in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,9 +59,10 @@ dependencies {
 
 }
 
-
-
-
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
 
 tasks.test {
     useJUnitPlatform()
